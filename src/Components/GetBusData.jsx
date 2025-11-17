6d76aa5e3973900817093acf950eec36fde8437a
Balxkodehodet <balx042025@gmail.com>
2025-11-17 09:52:07 +0100

    Reverterte til server.js fil som backend da c# implementasjonen trengs å jobbes med før det fungerer

--- src/Components/GetBusData.jsx
+++ src/Components/GetBusData.jsx
@@ -3,14 +3,12 @@ import "../App.css";
 
 export default function GetBusData() {
   const [busData, setBusData] = useState("");
-  const [stopName, setStopName] = useState("Bergen");
-  const [bus, setBus] = useState("Bergen");
+  const [stopName, setStopName] = useState("");
+  const [bus, setBus] = useState("arna");
   console.log("Bus data from context:", busData);
   useEffect(() => {
     async function fetchBusData() {
-      const res = await fetch(
-        `http://localhost:5049/api/bus-departures-by-name?stopName=Bergen`
-      );
+      const res = await fetch(`http://localhost:3001/api/buss-data`);
       const data = await res.json();
       console.log("Data fetched of bus data", data);
       setBusData(data);
@@ -18,25 +16,15 @@ export default function GetBusData() {
     fetchBusData();
   }, []);
   let busdepartures = [];
-  let uniqueStops = [];
+  let busdepartures2;
   if (busData) {
-    busdepartures = busData.departures;
-    // Create a unique array of stops by stopName
-    uniqueStops = Array.from(
-      new Map(busdepartures.map((item) => [item.stopName, item])).values()
+    busdepartures = busData.data.stopPlace.estimatedCalls;
+    busdepartures2 = busdepartures.filter(
+      (bus) => bus.serviceJourney.line.transportMode === "BUS".toLowerCase()
     );
   }
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-    console.log("Form submitted, selected option:", event.target.value);
-    const bus = await busData.departures.find(
-      (station) => station.stopName === event.target.value
-    );
-    setBus(bus);
-  }
-
-  console.log(busdepartures);
+  console.log("bus departures: ", busdepartures2);
   console.log(busData);
   console.log("BUS:", bus);
   return (
@@ -45,57 +33,23 @@ export default function GetBusData() {
         <p>Laster data...</p>
       ) : (
         <>
-          <div className="bus-formdata">
-            <h2>Buss stopper:</h2>
-            <form onSubmit={handleSubmit}>
-              <label>
-                Velg
-                <select value={bus} onChange={(e) => setBus(e.target.value)}>
-                  <option value="">Velg et stopp</option>
-                  {uniqueStops.map((d, index) => (
-                    <option key={d.stopId + index} value={d.stopName}>
-                      {d.stopName}
-                    </option>
-                  ))}
-                </select>
-              </label>
-              <label>
-                Søk etter buss stopp:
-                <input
-                  type="search"
-                  value={stopName}
-                  onChange={(e) => setStopName(e.target.value)}
-                />
-                {stopName &&
-                  uniqueStops.map((d, index) => (
-                    <option key={d.stopId + index} value={d.stopName}>
-                      {d.stopName}
-                    </option>
-                  ))}
-              </label>
-              <button type="submit">Søk</button>
-            </form>
-          </div>
           <div className="bus-data">
-            <h2>Velg</h2>
-            {busdepartures.map((bus, index) => (
+            <h2>Neste 15 busser avgang fra Bergen buss stasjon: </h2>
+            {busdepartures2.map((bus, index) => (
               <div key={index}>
                 <p>
-                  Buss til: <b>{bus.departure.destinationDisplay.frontText}</b>
+                  Buss til: <b>{bus.destinationDisplay.frontText}</b>
                   <br></br>
                   Forventet avgangstid:{" "}
                   <b>
-                    {bus.departure.expectedDepartureTime
+                    {bus.expectedDepartureTime
                       .replace("T", " Kl:")
                       .replace("+01:00", " ")}
                   </b>
                   <br></br>
                   Linje:
                   <b>
-                    {bus.departure.serviceJourney.line.id.replace(
-                      "SKY:Line:",
-                      ""
-                    ) + " "}
+                    {bus.serviceJourney.line.id.replace("SKY:Line:", "") + " "}
                   </b>
                 </p>
                 <hr></hr>
